466bff140695fa88634b3067b8b7f9173930c2a7
kylzl <lk.roa@mlgcl.edu.ph>
2024-12-18 11:38:54 +0800

    d-rep modified

--- resources/views/admin/reports/daily.blade.php
+++ resources/views/admin/reports/daily.blade.php
@@ -72,7 +72,7 @@
                         <tbody>
                             <tr>
                                 <td>07:30 AM</td>
-                                <td>Mr. Mark Joseph C. Gigante</td>
+                                <td>MR. MARK JOSEPH C. GIGANTE</td>
                                 <td>IT ELEC 102<br> PC 114<br> PC 114</td>
                                 <td>askdhifhiurksg<br> fkhsgiuwrkji<br> fkhsgiuwrkji</td>
                                 <td> 07:30 AM - 09:00 PM at 103<br>
@@ -84,18 +84,18 @@
 
                             <tr>
                                 <td>03:00 PM</td>
-                                <td>Ms. Clara Rose</td>
+                                <td>MR. MARVIN BROÑOLA</td>
                                 <td>ENG 101<br> PC 118</td>
                                 <td>noteshere<br> anothernote</td>
-                                <td>03:00 PM - 04:30 PM at 401<br>06:30 PM - 08:00 PM at 402</td>
-                                <td>401<br>402</td>
+                                <td>03:00 PM - 04:30 PM at 201<br>06:30 PM - 08:00 PM at 202</td>
+                                <td>201<br>202</td>
                             </tr>
 
                             <tr>
 
                             <tr>
                                 <td>09:00 AM</td>
-                                <td>Ms. Jane Doe</td>
+                                <td>DR. APOLONIA SUAREZ</td>
                                 <td>CS 201<br> PC 115</td>
                                 <td>xyzexampletext<br> anotherexample</td>
                                 <td>09:00 AM - 10:30 AM at 201<br>01:00 PM - 02:30 PM at 202</td>
@@ -104,7 +104,7 @@
                             </tr>
                             <tr>
                                 <td>10:30 AM</td>
-                                <td>Dr. John Smith</td>
+                                <td>MR. ALAN ABERILLA</td>
                                 <td>MATH 203<br> PC 116</td>
                                 <td>loremipsumtext<br> samplenotes</td>
                                 <td>10:30 AM - 12:00 PM at 204<br>03:00 PM - 04:30 PM at 205</td>
@@ -112,20 +112,20 @@
                             </tr>
                             <tr>
                                 <td>01:00 PM</td>
-                                <td>Mr. Alex Tan</td>
+                                <td> MR. EMANUELLE BARRIENTOS</td>
                                 <td>PHYSICS 101<br> PC 117</td>
                                 <td>exampletext1<br> exampletext2</td>
-                                <td>01:00 PM - 02:30 PM at 301<br>05:00 PM - 06:30 PM at 302</td>
-                                <td>301<br>302</td>
+                                <td>01:00 PM - 02:30 PM at 203<br>05:00 PM - 06:30 PM at 302</td>
+                                <td>203<br>302</td>
                                 
                             </tr>
                             <tr>
                                 <td>03:00 PM</td>
-                                <td>Ms. Clara Rose</td>
+                                <td>MR. MARNITO C. MAHINLO</td>
                                 <td>ENG 101<br> PC 118</td>
                                 <td>noteshere<br> anothernote</td>
-                                <td>03:00 PM - 04:30 PM at 401<br>06:30 PM - 08:00 PM at 402</td>
-                                <td>401<br>402</td>
+                                <td>03:00 PM - 04:30 PM at 201<br>06:30 PM - 08:00 PM at 202</td>
+                                <td>201<br>202</td>
                             </tr>
 
                         </tbody>
