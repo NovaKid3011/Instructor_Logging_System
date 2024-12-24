122b575d6ca6775cc76f186f9a95fec7a99fd654
NovaKid3011 <mayorleanni@gmail.com>
2024-12-24 19:22:57 +0800

    conflict

--- routes/console.php
+++ routes/console.php
@@ -10,6 +10,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
+Schedule::command('app:clear-weekly-photo')->everyMinute();
 
 Schedule::call(function() {
     Mail::to('admin@example.com')->send(new PostMail());
